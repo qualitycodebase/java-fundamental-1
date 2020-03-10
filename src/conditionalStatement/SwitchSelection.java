47a9c4cd10aaeb8417d92c31836ebef0b5e18ca4
qualitycodebase <qualitycodebase@gmail.com>
2020-03-09 22:59:01 -0400

    updates

--- src/conditionalStatement/SwitchSelection.java
+++ src/conditionalStatement/SwitchSelection.java
@@ -1,15 +1,22 @@
 package conditionalStatement;
 
+/*
+ * 
+ *Break --> Breaking point if the statement is gets true.
+ *continue -- > The continue statement breaks one iteration (in the loop), if a specified condition occurs, 
+ *and continues with the next iteration in the loop.
+ */
 public class SwitchSelection {
 
 	public static void main(String[] args) {
 		
-		String browser_type = "IE";
+		String browser_type = "safari";
 		
 		switch(browser_type) {
 		
 		case "chrome":
 			System.out.println("This is chrome");
+			break;
 		case "firefox":
 			System.out.println("This is firefox");
 			break;
